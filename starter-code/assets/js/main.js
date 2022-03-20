f37a871ca4278bae4051e44ac562128e5a1c76e8
Murat Cinek <77546752+muratc34@users.noreply.github.com>
2022-03-20 15:21:13 +0300

    navbar added for mobile devices

--- starter-code/assets/js/main.js
+++ starter-code/assets/js/main.js
@@ -1,18 +1,28 @@
-/* SHOW MENU */
+/* MENU SHOW */
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
 
-/* Open Menu */
+/* Show Menu */
 if(navToggle){
     navToggle.addEventListener('click',()=>{
         navMenu.classList.add('show-menu');
     });
 }
 
-/* Close Menu */
+/* Hidden Menu */
 if (navClose) {
     navClose.addEventListener('click', ()=>{
         navMenu.classList.remove('show-menu');
     });
 }
+
+/* Remove Menu */
+// const navLink = document.querySelectorAll('.nav_link');
+
+// function linkAction(){
+//     const navMenu = document.getElementById('nav-menu');
+    
+//     navLink.classList.remove('show-menu');
+// }
+// navLink.forEach(n=> n.addEventListener('click', linkAction()));
